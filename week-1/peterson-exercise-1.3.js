19b00bac294c160aab6d034bf68778ea4adf1302
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-02-20 11:56:02 -0500

    finalized code for cell phone display and code comments

--- week-1/peterson-exercise-1.3.js
+++ week-1/peterson-exercise-1.3.js
@@ -8,3 +8,39 @@ var header = require('../header.js');
 ; Description: Exercise 1.3 - Function Class and Object Literal
 ;===========================================
 */ 
+
+/*
+FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ -- DISPLAYING CELL PHONE DETAILS --
+ Manufacturer: <manufacturer>
+ Model: <model>
+ Color: <color>
+ Price: <price>
+
+
+*/
+
+
+// start program
+
+
+function Cellphone(details) {
+    this.details = details;
+    this.manufacturer = "T-Mobile";
+    this.model = "Iphone 8";
+    this.color = "Gold";
+    this.price = "$" + 899.99;
+
+    console.log("-- DISPLAYING CELL PHONE DETAILS --");
+
+    this.getDetails = function() {
+        return this.details + " Manufacturer: " + this.manufacturer + "\n" + " Model: " + this.model + "\n" + " Color: " + this.color + "\n" + " Price: " + this.price;
+    };
+}
+var cellphone = new Cellphone("");
+
+console.log(cellphone.getDetails());
+
