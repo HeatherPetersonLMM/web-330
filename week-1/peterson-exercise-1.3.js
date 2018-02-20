0aa24743c7080586ed949eda457c63b9e459ae5b
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-02-20 11:08:14 -0500

    added header and code attribution

--- week-1/peterson-exercise-1.3.js
+++ week-1/peterson-exercise-1.3.js
@@ -1,23 +1,10 @@
-var myFirstName = "Heather";
-var myLastName = "Peterson";
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1;
-var yyyy = today.getFullYear();
-
-if (dd<10) {
-    dd = '0' + dd
-  }
-  
-  if (mm<10) {
-    mm = '0' + mm
-  }
-  
-  today = mm + '-' + dd + '-' + yyyy
-  var assignmentNum = "Assignment 2.4";
-  
-  var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
-      + assignmentNum + "\nDate: " + today;
-  
-  console.log(programHeader);
-  console.log("\n");
+var header = require('../header.js'); 
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   20 January 2018
+; Modified By: Heather Peterson
+; Description: Exercise 1.3 - Function Class and Object Literal
+;===========================================
+*/ 
