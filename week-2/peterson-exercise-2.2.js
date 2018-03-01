873d1cc13110298514df2cc66d35f358f72daff8
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-03-01 09:58:10 -0500

    Completed code with code attributions and comments

---
+++ week-2/peterson-exercise-2.2.js
@@ -0,0 +1,34 @@
+var header = require('../header.js'); 
+/*
+============================================
+; Title:  Prototype Pattern
+; Author: Professor Krasso 
+; Date:   1 March 2018
+; Modified By: Heather Peterson
+; Description: Exercise 2.2
+;===========================================
+*/ 
+
+var Person =  //creating a person object 
+{
+    getAge: function()  // creating an age function to return an age
+    {
+        return this.age;  //returns age
+    }
+};
+
+var Heather = Object.create(Person, {  //creating a person object using the Person prototype
+    "age":  //body creates age with a value of "31"
+    {
+        "value": "31"
+    },
+        "fullname":  //body creates a full name with a value of "Heather Peterson"
+    {
+        "value": "Heather Peterson"
+    }
+});
+
+Heather.getAge();  //calling the getAge function of Heather object
+
+console.log("The person's full name is: '%s'", Heather.fullname + "."); //outputs the full name value
+console.log("The person's age is: '%s'", Heather.age + "."); //outputs the age value
